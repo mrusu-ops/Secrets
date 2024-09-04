4507a361c43aea7b0a8fe51d215c4b7dfea45027
mrusu-ops <melania.rusu@opswat.com>
2024-09-04 15:15:49 +0300

    Delete main.java

--- main.java
+++
@@ -1,9 +0,0 @@
-static void main(String[] args) {
-  String endpoint = "https://datahub.cn-shanghai-shga-d01.dh.alicloud.ga.sh";
-  String accessId = "0iWVONCs805VuAAu"
-  String accessKey = "iEwlgpCnXDwT93YMVDb2G60my9ne81"
-  String projectName = "sjc_rwzx";
-  String topicName = "task_center_platform_request";
-
-  RecordSchema schema = new RecordSchema();
-}
