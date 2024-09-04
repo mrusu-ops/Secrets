0e91abbbd58dde608b4db6cc008075abfcaa6b9c
mrusu-ops <melania.rusu@opswat.com>
2024-09-04 11:24:52 +0300

    Update main.java

--- main.java
+++ main.java
@@ -1,9 +1,9 @@
 static void main(String[] args) {
   String endpoint = "https://datahub.cn-shanghai-shga-d01.dh.alicloud.ga.sh";
-  String accessId = "0dsafjhu8iJhued47R"
-  String accessKey = "iEhgGTYvfVDS574sNH"
-  String projectName = "shc_jsahg";
-  String topicName = "task+center_platform_request";
+  String accessId = "0iWVONCs805VuAAu"
+  String accessKey = "iEwlgpCnXDwT93YMVDb2G60my9ne81"
+  String projectName = "sjc_rwzx";
+  String topicName = "task_center_platform_request";
 
   RecordSchema schema = new RecordSchema();
 }
