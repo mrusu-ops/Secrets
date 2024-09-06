be8f45eac35e196a54c448d8e673cf831fdaabd0
mrusu-ops <melania.rusu@opswat.com>
2024-09-06 16:33:12 +0300

    Update bucket_s3.py

--- bucket_s3.py
+++ bucket_s3.py
@@ -10,6 +10,10 @@ def aws_upload(data: Dict):
     database = aws_lib.connect("AKIAF6BAFJKR45SAWSZ5", "hjshnk5ex5u34565AWS654/JKGjhz545d89sjkja")
     database.push(data)
 
+def aws_upload(data: Dict):
+    database = aws_lib.connect("AKIAF6BAFJKR45SAWSZ5", "hjshnk5ex5u34565AWS654/JKGjhz545d89sjkja")
+    database.push(data)
+
 
 def transform_data(es_data: Dict) -> Dict:
     es_data = {**data, "origin": "ES"}
